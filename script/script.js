b0863bac42ba537864ac1717d14da2a3fd6d3216
benschuite <ben.schuite00@gmail.com>
2021-03-01 11:15:14 +0100

    Challenge1
    Challenge1

---
+++ script/script.js
@@ -0,0 +1,52 @@
+var ctx = document.getElementById('Brandstof').getContext('2d');
+var gradientStroke = ctx.createLinearGradient(500, 0, 100, 0);
+gradientStroke.addColorStop(0, "#1BADCF");
+gradientStroke.addColorStop(1, "#21D27F");
+
+var chart = new Chart(ctx, {
+    type: 'doughnut',
+	data: {
+        datasets: [{
+            label: 'Brandstof',
+            data: [80, 20],
+            backgroundColor: [gradientStroke, '#3E434B'] ,
+			borderWidth: 0,
+			cutoutPercentage: 20     
+		}]
+    },
+
+    options: {
+    	aspectRatio: 1.5,
+    	cutoutPercentage: 80,
+    }
+});
+
+
+var ctx2 = document.getElementById('snelheid').getContext('2d');
+var gradientStroke = ctx2.createLinearGradient(500, 0, 100, 0);
+gradientStroke.addColorStop(0, 'rgba(33, 210, 127, 0.1)');
+gradientStroke.addColorStop(1, 'rgba(27, 173, 207, 0.6)');
+
+var chart2 = new Chart(ctx2, {
+    type: 'line',
+
+    data: {
+        labels: ['January', 'February', 'March', 'April', 'May'],
+        datasets: [{
+        	label: 'Snelheid',
+            backgroundColor: gradientStroke,
+            borderColor: gradientStroke,
+            data: [0, 8, 13, 14, 16]
+        }]
+    },
+
+    options: {
+    	aspectRatio: 1.5,
+    	legend: {
+        	display: false
+    	}
+
+    }
+});
+
+
